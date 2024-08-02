7fe1479ccbbea5c3b0d58946c81498d98448c52f
Afnan7120 <afnanmohamed7120@gmail.com>
2024-08-02 20:36:09 +0300

    NavBar,login and Register Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './pages/Navbar/Navbar';
+import LoginInfo from './pages/LoginPage/LoginPage';
+import RegisterInfo from './pages/RegisterPage/RegisterPage';
+import NotFound from './pages/NotFound/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/Login" element={<LoginInfo />} />
+        <Route path="/Register" element={<RegisterInfo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
