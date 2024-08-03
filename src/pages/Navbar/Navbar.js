1591753499dcfec2d946dbad272da786a7f76acd
amrelsaady98 <amrelsaady98@hotmail.com>
2024-08-03 15:58:32 +0300

    carousel & nav bar completed @ home_page.js, Navbar.js

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import { Container, AppBar, Toolbar, Typography, Button} from '@mui/material';
 import { Link } from 'react-router-dom';
+import DefaultNavbar from "../../examples/Navbars/DefaultNavbar";
+import routes from "../../routes";
 
 const Navbar = () => {
  
   return (
-    <Container>
-      <AppBar position="static" color=''>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1,
-               textShadow: '4px 2px 5px rgba(6,0,0,0.3), 1px 6px 2px rgba(6,5,0,0.2)' }}>
-            Skillify
-          </Typography>
-          <nav>
-            <Link to="/" style={{ marginRight: 20 }}>
-                Home
-            </Link>
-            <Link to="/about" style={{ marginRight: 20 }}>
-                About
-            </Link>
-            <Link to="/contact" style={{ marginRight: 20 }}>
-                Contact
-            </Link>
-          </nav>
-          <Button variant="text" color="secondary" style={{ marginLeft: 20 }} component={Link} to="/Login">
-            Login
-          </Button>
-          <Button variant="text" color="secondary" style={{ marginLeft: 20 }} component={Link} to="/Register">
-            Register
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Container>
+      <DefaultNavbar
+          brand={'Skillify'}
+          routes={routes}
+          sticky
+      />
+    // <Container>
+    //   <AppBar position="static" color=''>
+    //     <Toolbar>
+    //       <Typography variant="h6" component="div" sx={{ flexGrow: 1,
+    //            textShadow: '4px 2px 5px rgba(6,0,0,0.3), 1px 6px 2px rgba(6,5,0,0.2)' }}>
+    //         Skillify
+    //       </Typography>
+    //       <nav>
+    //         <Link to="/" style={{ marginRight: 20 }}>
+    //             Home
+    //         </Link>
+    //         <Link to="/about" style={{ marginRight: 20 }}>
+    //             About
+    //         </Link>
+    //         <Link to="/contact" style={{ marginRight: 20 }}>
+    //             Contact
+    //         </Link>
+    //       </nav>
+    //       <Button variant="text" color="secondary" style={{ marginLeft: 20 }} component={Link} to="/Login">
+    //         Login
+    //       </Button>
+    //       <Button variant="text" color="secondary" style={{ marginLeft: 20 }} component={Link} to="/Register">
+    //         Register
+    //       </Button>
+    //     </Toolbar>
+    //   </AppBar>
+    // </Container>
   );
 };
 
--- src/pages/home/home_page.js
+++ src/pages/home/home_page.js
@@ -1,15 +1,104 @@
-import MKInput from "../../components/MKInput";
-import MKBadge from "../../components/MKBadge";
-import img from 'assets/images/city-profile.jpg'
-import DefaultNavbar from "../../examples/Navbars/DefaultNavbar";
+
+import {Container, Paper, Slider} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import routes from "../../routes";
-import DarkNavBar from "../../components/NavBar/DarkNavBar";
+import {Image} from "@mui/icons-material";
+import {blue} from "@mui/material/colors";
+import pxToRem from "../../assets/theme/functions/pxToRem";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
 
 export default function HomePage(){
+
+    var items = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!",
+            image:'alx-slide-10.png'
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!",
+            image:'alx-slide-9.png'
+
+        },
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!",
+            image:'alx-slide2-min.png'
+        }
+    ]
+
     return (
         <>
-            <DarkNavBar/>
+            <Carousel
+                indicators={true}
+                indicatorContainerProps={{
+                   style:{
+                       zIndex: 3,
+                       top: "-10rem",
+                       marginBottom:'-4rem',
+                       position: "relative",
+                   }
+                }}
+                indicatorIconButtonProps={{
+                    style:{
+                        scale:'0.7'
+                    }
+                }}
+            >
+               {
+                   items.map( (item, i) => (<Item item={item} key={i} />))
+               }
+            </Carousel>
+            <Container
+                sx={{
+                    display:{xs:'none', lg:'block'},
+                    width: "100%",
+                    height:'7rem',
+                    backgroundColor:blue[200],
+                    position:'relative',
+                    top:'-3.5rem',
+                    zIndex:'5',
+                    borderRadius:pxToRem(4),
+                }}
+            >
+
+            </Container>
+
+            <Box
+                sx={{
+                    px:'0',
+                    py:'12px',
+                    borderBottom:'0.2rem solid blue',
+                    display:'inline'
+                }}
+            >
+                All
+            </Box>
         </>
     );
+}
+function Item(props)
+{
+    return (
+        <Box
+            sx={{
+                width:'100vw',
+                height:{xs: '330px', lg: '40vw'},
+            }}
+        >
+        <img
+            ci
+            src={require(`assets/images/carousel/${props.item.image}`)}
+
+            style={{
+                objectFit: 'cover',
+                width:'100%'
+            }}
+        />
+
+        </Box>
+    )
 }
