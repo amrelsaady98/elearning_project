ae1e816d952a40e61757012769e2a11a3e3be472
amrelsaady98 <amrelsaady98@hotmail.com>
2024-08-03 03:25:17 +0300

    mk added

---
+++ src/pages/home/home_page.js
@@ -0,0 +1,12 @@
+
+import MKBox from "../../components/MKBox";
+import MKInput from "../../components/MKInput";
+
+
+export default function HomePage(){
+    return (
+        <>
+            
+        </>
+    );
+}
